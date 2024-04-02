e785958f77a98005dd90a5660c7986f69d38fa90
vusalhero <qahramanli15@gmail.com>
2024-04-02 12:51:10 +0400

    new task added

---
+++ js-task 4/main.js
@@ -0,0 +1,58 @@
+// Task-1
+// let team = ["Liverpool", "arsenal", "Galatasaray"];
+// team [1] = "Real Madrid"
+// console.log(team);
+
+// Task-2
+// let arr = ['New Hope', 'The Empire Strikes Back', 'Return of the Jdi'];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(`Episode ${4+i} ${arr[i]}`);
+
+// }
+
+// Task-3
+// let salary = [
+//     [60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60],
+//     [75, 75, 75, 75, 75, 75, 70, 77, 75, 75, 70, 75],
+//     [150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 180],
+//     [65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65],
+//     [80, 80, 80, 80, 80, 80, 80, 105, 105, 105, 105, 105],
+//     [65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65],
+//     [80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 120],
+//     [65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65, 65],
+//     [80, 80, 80, 80, 80, 80, 80, 90, 90, 90, 90, 90],
+//     [75, 75, 75, 75, 75, 75, 75, 75, 75, 75, 70, 75]
+// ];
+
+// let totalSalary=0
+
+// for (let i = 0; i < salary.length; i++) {
+//        let userSalary = salary[i]
+// for (let j = 0; j < userSalary.length; j++) {
+    
+//     totalSalary+= salary[j]
+// }    
+// }
+
+// console.log(totalSalary);
+
+// Task-4
+// const nums = `32,31,34,36,30`
+// let numArr = nums.split(",")
+
+// let minNum = +numArr[0]
+// for (let i = 1; i < numArr.length; i++) {
+//     if (+numArr[i]<minNum) {
+//         minNum = numArr[1]
+//     }
+
+// }
+// console.log(minNum);
+
+// task-5
+
+// let nums= `32,31,34,36,30`
+// let numArr = nums.split(",")
+// nums = numArr.join(",")
+// console.log(nums);
