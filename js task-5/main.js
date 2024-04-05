5927e107ff94d3784cf44d99358dde3e8ec5fc0e
vusalhero <qahramanli15@gmail.com>
2024-04-05 11:39:00 +0400

    new task added

---
+++ js task-5/main.js
@@ -0,0 +1,65 @@
+// Task-1
+// let aboutObj={
+//     username: "Vusal",
+//     surname: "Gahramanli",
+//     gender: "Male",
+//     birthday: "01-05-1997"
+// }
+
+// console.log(aboutObj.birthday);
+
+// task-2
+// let aboutObj={
+//     username: "Vusal",
+//     surname: "Gahramanli",
+//     gender: "Male",
+//     birthday: "01-05-1997"
+// }
+
+// delete aboutObj.birthday;
+// aboutObj.age = "26"
+
+// console.log(aboutObj);
+
+
+// task-4
+// const creditCard = {
+//     fullName: "Kərim",
+//     creditLimit: 600,
+//     balance: 345,
+//     //   precentOfMinPayment: ,
+//     getBalance: function () {
+//       if (this.balance < 0) {
+//         return Borcunuz ${this.balance};
+//       } else {
+//         return Balansınız ${this.balance};
+//       }
+//     },
+//     getMinPaymant: function () {
+//       if (this.precentOfMinPayment > 0) {
+//         return Minimum ödənişiniz ${precentOfMinPayment};
+//       } else {
+//         return "Sizin borcunuz yoxdur";
+//       }
+//     },
+//     withDraw: function (pay) {
+//       if (this.balance > pay) {
+//         this.balance -= pay;
+//         return cari balans ${this.balance};
+//       } else if (this.balance + this.creditLimit > pay) {
+//         this.creditLimit -= pay - this.balance;
+//         this.balance = 0;
+//         return balansınız ${this.balance} kredit limitiniz ${this.creditLimit};
+//       } else {
+//         return ` balansınızda vəsait azdır \r\n Balansınız: ${this.balance + this.creditLimit} `;
+//       }
+//     },
+//     refill: function (pay ) {
+//       this.balance += pay;
+//     },
+//   };
+  
+//   creditCard.refill(345)
+//   console.log(creditCard.getBalance());
+//   console.log(creditCard.withDraw(4000));
+
