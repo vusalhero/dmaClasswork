9e020d31f589bfc2e3a756e14a3e16f36aba4518
vusalhero <qahramanli15@gmail.com>
2024-05-15 12:07:38 +0400

    task 2,task3

---
+++ task2/main.js
@@ -0,0 +1,20 @@
+// task 1
+
+// function swapCase(text) {
+//     let swappedText = "";
+//     for (let i = 0; i < text.length; i++) {
+//       let char = text[i];
+//       if (char === char.toUpperCase()) {
+//         swappedText += char.toLowerCase();
+//       } else {
+//         swappedText += char.toUpperCase();
+//       }
+//     }
+//     return swappedText;
+//   }
+
+//   console.log(swapCase("aBcD")); // Output: "AbCd"
+// console.log(swapCase("jAvaScRIPt")); // Output: "JaVAsCripT"
+// console.log(swapCase("jeDAcAdemY")); // Output: "JEdaCaDEMy"
+
+
